07df94b294e4c77bac4a3d590dcb8251d734f7b0
typokills <chinstermojo@gmail.com>
2020-04-19 16:58:17 +0800

    Final

--- ChinToken.sol
+++ ChinToken.sol
@@ -5,8 +5,12 @@ contract ChinToken{
     uint256 totalSupply = 0;
     address tokenIssuer;
     mapping (address => bool) approval;
+    mapping (address => address) approvalAcc;
     
-    
+    constructor() public{
+        tokenIssuer = msg.sender; //Only token issuer can create new tokens
+    }
+
     //Can only check balance and everyone can use it
     function getBalance(address _account) public view returns (uint256){
         return tokenBalance[_account];
@@ -20,8 +24,8 @@ contract ChinToken{
     //Creates the initial pool of tokens
     function IssueTokens(uint256 _startingSupply) public{
         //all of it goes into  contract creator's wallet
+        require(msg.sender == tokenIssuer);
         tokenBalance[msg.sender] = _startingSupply;
-        tokenIssuer = msg.sender;
         totalSupply = _startingSupply;
     }
 
@@ -36,6 +40,7 @@ contract ChinToken{
     //Third party transfer of tokens, Can only be used by issuer of the tokens
     function transferFrom(address _giver, address _receiver, uint256 amt) public {
         require(tokenBalance[_giver] >= amt);
+        require(approvalAcc[_giver] == msg.sender) 
         require(approval[_giver] == true); //_giver must give prior approval for transaction
         require(amt > 0); //ensures sender cannot steal
 
@@ -46,14 +51,19 @@ contract ChinToken{
     }
 
     //Function for user to give prior approval for 3rd party transaction
-    function giveApproval (bool choice) public {
+    function giveApproval (bool choice, address thirdPty) public {
         approval[msg.sender] = choice;
+        approvalAcc[msg.sender] = thirdPty;
     }
+
 
     // For third party to check before making 3rd party transfer
     function viewApproval(address _account) public view returns (bool){
         return approval[_account];
     }
     
+    function getIssuer() public view returns (address){
+        return tokenIssuer;
+    }
     
 }
